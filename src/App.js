1cf4af93eb510bd44d8256392e22ad4acdbb7b1b
twlai251 <twlai251@gmail.com>
2020-02-11 20:21:22 -0500

    Adding CSS and the increment/decrement/clear Method- tested and working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Counter extends Component{
+  constructor(){
+    super();
+    this.state = {
+      count:0,
+    }
+  }
+
+
+  //this is where we are going to put out methods
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1,
+    })
+  }
+
+  //Decrement
+  decrement = () => {
+    this.setState({
+      count: this.state.count - 1,
+    })
+  }
+
+  //clear
+  clearButton = () => {
+    this.setState({
+      count: 0,
+    })
+  }
+
+
+  render(){
+    return(
+      <div className="container">
+
+        <div className="navbar">Counter</div>
+
+        <div className="counter">
+
+          <h1>{this.state.count}</h1>
+          <button type="button" onClick={this.increment}> Increment</button>
+          <button type="button"onClick={this.decrement}> Decrement</button>
+          <button type="button"onClick={this.clearButton}> Clear</button>
+
+        </div>
+
+      </div>
+    )
+  }
 }
 
-export default App;
+export default Counter;
